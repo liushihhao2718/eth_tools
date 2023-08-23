2c954fee712e30d3bb8f60a0842db10953ce632e
Liu shih hao <tom1_liu@asus.com>
2023-08-23 17:15:28 +0800

    更新decode工具

--- eth_tool/src/views/TranscationData.vue
+++ eth_tool/src/views/TranscationData.vue
@@ -9,6 +9,7 @@ const abi_text = ref(
   `[{"constant":false,"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"approve","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"mint","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"bytes","name":"_data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"bool","name":"approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"transferFrom","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"approved","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":false,"internalType":"bool","name":"approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},{"constant":true,"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"getApproved","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"}]`
 );
 
+const parseType = ref('transcation');
 const parsed_data = ref();
 const error_msg = ref();
 const signature = computed(() =>
@@ -20,7 +21,20 @@ function parseInputData(abi, data) {
       keys.filter((key) => key in obj).map((key) => [key, obj[key]])
     );
   const iface = new Interface(abi);
-  const parsed = iface.parseTransaction({ data });
+  let parsed;
+
+  switch (parseType.value) {
+    case 'transcation':
+      parsed = iface.parseTransaction({ data });
+      break;
+    case 'log':
+    parsed = iface.parseLog(data);
+      break;
+    case 'error':
+    parsed = iface.parseError(data);
+      break;
+  }
+
   const table = parsed.fragment.inputs.map((x, i) =>
     Object.assign(pick(x, "name", "type"), { value: parsed.args[i].toString() })
   );
@@ -52,22 +66,14 @@ function clearView() {
     <h2>ABI</h2>
     <v-textarea label="" variant="outlined" v-model="abi_text"></v-textarea>
     <h2>Data</h2>
-    <v-textarea
-      label=""
-      rows="3"
-      variant="outlined"
-      v-model="data"
-    ></v-textarea>
+    <v-textarea label="" rows="3" variant="outlined" v-model="data"></v-textarea>
+
+    <v-select v-model="parseType" :items="['transcation', 'log', 'error']" label="Type" required></v-select>
     <v-btn variant="tonal" @click="decode()"> Decode </v-btn>
 
     <v-divider :style="{ marginTop: '30px', marginBottom: '30px' }"></v-divider>
 
-    <v-alert
-      v-if="error_msg"
-      :text="error_msg"
-      color="red-lighten-1"
-      type="error"
-    ></v-alert>
+    <v-alert v-if="error_msg" :text="error_msg" color="red-lighten-1" type="error"></v-alert>
     <div v-else-if="parsed_data">
       <v-spacer class="d-flex" style="align-items: center">
         <h2>Signature</h2>
